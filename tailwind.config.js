bc59d3ec2cc32e2b0194f8135a22caf9c5f8ab88
Ëduardo Cabrera <edumcabrera@gmail.com>
2025-04-07 00:33:42 +0200

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        taupe: '#7D6B5F',
+        grayish: '#9E9396',
+        lavender: '#9C93B0',
+        steel: '#98A7C4',
+        cloud: '#CACACC',
+        softblue: '#BFD9E8',
+      },
+    },
   },
   plugins: [],
 }
